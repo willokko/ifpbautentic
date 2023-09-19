a9ad92a74e6247da06c84d15cf66aece0bedf2bf
José Wilson Lins Pereira <20211230002@ad.ifpb.edu.br>
2023-09-19 14:26:03 -0300

    up

---
+++ screens/telalogin.js
@@ -0,0 +1,13 @@
+///rnfs
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function telalogin() {
+  return (
+    <View>
+      <Text>telalogin</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
